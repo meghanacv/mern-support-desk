1bddfe7ab4b464cddf09796578c64364b5d75d1c
Meghana C V <meghana.cvmcvm@gmail.com>
2024-04-04 12:04:15 +0530

    add single ticket and close ticket functionality

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -12,15 +12,11 @@ function Tickets() {
 
   const dispatch = useDispatch()
 
-  // NOTE: only need one useEffect here
 
   useEffect(() => {
     dispatch(getTickets())
   }, [dispatch])
 
-  // NOTE: no need for loading state, we can check for absence of tickets
-  // If we don't have tickets we are loading, if we do have tickets we just
-  // need to update the tickets with latest tickets in the background
   if (!tickets) {
     return <Spinner />
   }
